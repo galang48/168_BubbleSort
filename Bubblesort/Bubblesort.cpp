d8f725e950662876ee28736e309cd51a2e605596
galang48 <galangyudha1945@gmail.com>
2024-03-07 11:52:37 +0700

    membuat prosedur bubblesort array

--- Bubblesort/Bubblesort.cpp
+++ Bubblesort/Bubblesort.cpp
@@ -39,11 +39,22 @@ void display() {            // Prosedur umtuk menampilkan hasil
 
         cout << endl;                           //Output baris kosong
     }
+}
+void bubbleSortArray() {
+    int pass = 1;
 
+    for (pass; pass <= n - 1; pass + 1) {               //Looping dengan i dimulai dari 1 hingga n-1
+        for (int j = 0; j <= n - 1 - pass; j++) {       //Looping dengan j dimulai dari 0 hingga n-1
+            if (a[j] > a[j + 1]) {                      //Jika nilai pada a[j] lebih besar dari a[j+1]
+                int temp = a[j];                        //Simpan nilai a[j] ke variabel sementara temp
+                a[j] = a[j + 1];                        //Assign nilai a[j+1] ke a[j]
+                a[j + 1] = temp;                        //Assign nilai temp ke a[j+1]
+            }
+        }
+    }
 }
 
-int main()
-{
+int main() {
     std::cout << "Hello World!\n";
 }
 
